3590380bbbb7cd6dc6b145fc09ec0c2a18a973ae
dmitrii92 <fogot92@gmail.com>
2020-10-12 13:18:09 +0300

    feat: add isLoading

--- src/features/wolfram/WolframPlot.tsx
+++ src/features/wolfram/WolframPlot.tsx
@@ -3,18 +3,23 @@ import { useSelector } from "react-redux";
 import { selectExpression, selectMax, selectMin } from "../calculator/calculatorSlice";
 import { getGraph } from "./api/Wolfram";
 import { WolframResponeDto } from "./api/WolframResponse";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const WolframPlot = () => {
   const expression = useSelector(selectExpression);
   const min = useSelector(selectMin);
   const max = useSelector(selectMax);
   const [wolframResult, setWolframResult] = useState<WolframResponeDto>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     let cleanupFunction = false;
     if (expression) {
+      setIsLoading(true);
+      setWolframResult(null);
       getGraph(`${expression}, x = ${min} to ${max}`).then((result) => {
         if (!cleanupFunction) {
+          setIsLoading(false);
           setWolframResult(result);
         }
       });
@@ -24,6 +29,7 @@ const WolframPlot = () => {
 
   return (
     <div style={{ padding: "10px" }}>
+      {isLoading ? <CircularProgress /> : null}
       {wolframResult ? (
         <img
           src={wolframResult.imgSrc}
